0611092ef675321372df9f8034474f23b061e97f
Luca Dragoni <luca.dragoni2006@gmail.com>
2023-12-07 23:28:05 +0100

    aggiunto messaggio per username o password errata in fase di login

--- js/login.js
+++ js/login.js
@@ -6,11 +6,18 @@ $(document).ready(function() {
     }
 
     $("#button").click(function() {
-        let username = $("#username").val();
-        let password = $("#password").val();
-        if(username === "luca" && password === "1234") {
+        let username = $("#username");
+        let password = $("#password");
+        if(username.val() === "luca" && password.val() === "1234") {
             document.cookie = "username=luca; expires=Thu, 01 Jan 2025 00:00:00 UTC; path=/;"
             window.location.href = "netid.html";
+        }
+        else {
+            $("p").remove();
+            const paragraph1 = $($("<p>Invalid username or password. Retry.</p>"));
+            const paragraph2 = $($("<p>Invalid username or password. Retry.</p>"));
+            username.after(paragraph1);
+            password.after(paragraph2);
         }
     });
 });
